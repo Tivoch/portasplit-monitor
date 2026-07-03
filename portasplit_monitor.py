ff5118d6d69b403615ff9628f08b0158276d9519
Tivoch <47317878+Tivoch@users.noreply.github.com>
2026-07-03 09:05:31 +0200

    Clear out of stock messages for a product
    Removed out of stock messages for Amazon OLIMPIA SPLENDID.

--- portasplit_monitor.py
+++ portasplit_monitor.py
@@ -87,8 +87,7 @@ STORES = [
         "name": "Amazon OLIMPIA SPLENDID",
         "url": "https://www.amazon.fr/dp/B083X59QZF/",
         "in_stock":     ["ajouter au panier", "add to cart"],
-        "out_of_stock": ["actuellement indisponible", "currently unavailable",
-                         "en rupture de stock"],
+        "out_of_stock": [],
     },
 ]
 
